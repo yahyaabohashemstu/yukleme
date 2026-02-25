2fb228c732181232251cd1aa3c0e9bba65d78ba3
yahyaabohashemstu <212802099@ogr.cbu.edu.tr>
2026-02-25 14:50:40 +0300

    search section

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@
 // Version: 1.0.0
 // ============================================
 
-const CACHE_NAME = 'yukleme-cache-v12';
-const DYNAMIC_CACHE = 'yukleme-dynamic-v12';
+const CACHE_NAME = 'yukleme-cache-v13';
+const DYNAMIC_CACHE = 'yukleme-dynamic-v13';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [

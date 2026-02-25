98e4ac0dfeeb46fe96e976ef79af0c9f5b4cdb15
yahyaabohashemstu <212802099@ogr.cbu.edu.tr>
2026-02-25 13:33:43 +0300

    Refactor: Remove weight section, move vehicle weight to products section; rename labels

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@
 // Version: 1.0.0
 // ============================================
 
-const CACHE_NAME = 'yukleme-cache-v11';
-const DYNAMIC_CACHE = 'yukleme-dynamic-v11';
+const CACHE_NAME = 'yukleme-cache-v12';
+const DYNAMIC_CACHE = 'yukleme-dynamic-v12';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [

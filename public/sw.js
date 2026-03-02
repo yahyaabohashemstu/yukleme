7e137793fc8770a657516599ed950453272222ef
yahyaabohashemstu <212802099@ogr.cbu.edu.tr>
2026-03-02 11:30:40 +0300

    numbers sum

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@
 // Version: 1.0.0
 // ============================================
 
-const CACHE_NAME = 'yukleme-cache-v19';
-const DYNAMIC_CACHE = 'yukleme-dynamic-v19';
+const CACHE_NAME = 'yukleme-cache-v20';
+const DYNAMIC_CACHE = 'yukleme-dynamic-v20';
 
 // Files to cache immediately on install
 const STATIC_ASSETS = [
